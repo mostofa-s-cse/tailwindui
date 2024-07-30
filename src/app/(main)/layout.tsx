b2525c8a99bb36679c94b9d8a9151d25d21e206e
mostofa-s-cse <mostofa.s.cse@gmail.com>
2024-07-30 10:18:00 +0600

    home page navbar add and section add

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import {Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/react";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,68 @@ export default function Layout({children,}: Readonly<{
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+        <Disclosure as="nav" className="bg-gray-800">
+                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <div className="flex h-16 items-center justify-between">
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                                <h2 className="text-2xl text-white font-bold">Tailwind UI</h2>
+                            </div>
+                        </div>
+                        <div className="hidden sm:ml-6 sm:block">
+                            <div className="flex space-x-4">
+                                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                                <a
+                                    href="/"
+                                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                >
+                                    Components
+                                </a>
+                                <a
+                                    href="#"
+                                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                >
+                                    Projects
+                                </a>
+
+                            </div>
+                        </div>
+                        <div className="-mr-2 flex sm:hidden">
+                            {/* Mobile menu button */}
+                            <DisclosureButton
+                                className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <span className="absolute -inset-0.5"/>
+                                <span className="sr-only">Open main menu</span>
+                                <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden"/>
+                                <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block"/>
+                            </DisclosureButton>
+                        </div>
+                    </div>
+                </div>
+
+                <DisclosurePanel className="sm:hidden">
+                    <div className="space-y-1 px-2 pb-3 pt-2">
+                        {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                        <DisclosureButton
+                            as="a"
+                            href="/"
+                            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+                        >
+                            Components
+                        </DisclosureButton>
+                        <DisclosureButton
+                            as="a"
+                            href="#"
+                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                        >
+                            Calendar
+                        </DisclosureButton>
+                    </div>
+                </DisclosurePanel>
+        </Disclosure>
+        {children}
+        </body>
         </html>
     );
 }
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,128 @@
+import Heroes from '@/app/images/heroes.png';
+import featureSections from '@/app/images/feature-sections.png';
+import ctaSections from '@/app/images/cta-sections.png';
+import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
     return (
         <div className="mx-auto max-w-7xl px-6 lg:px-8 sm:px-6">
-            <h2 className="mt-5 text-2xl font-bold">Tailwind UI</h2>
+            <div className="py-6">
+                <h2 className="text-3xl font-semibold text-black">Marketing</h2>
+                <p className="mt-4">Heroes, feature sections, newsletter sign up forms — everything you need
+                    to build beautiful marketing
+                    websites.</p>
+            </div>
+
+            <div>
+                <div className="relative bg-white py-8">
+                    {/* Hero Sections Start */}
+                    <div className="mx-auto">
+                        <h2 className="mt-2 text-2xl font-bold text-center tracking-tight text-gray-900">
+                            Page Sections
+                        </h2>
+
+                        <div className="mt-12 px-8">
+                            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+
+                                <Link href="/components/marketing/hero-section/">
+                                <div className="overflow-hidden rounded-lg shadow-lg">
+                                    <div className="px-2">
+                                        <Image src={Heroes} className="rounded-lg" alt="Heroes" />
+                                    </div>
+                                    <div className="bg-white py-3 px-3">
+                                        <p className="text-lg font-semibold text-gray-900">Hero Sections</p>
+                                        <p className="mt-1 text-sm text-gray-500">12 components</p>
+                                    </div>
+                                </div>
+                                </Link>
+
+
+                                <Link href="#">
+                                    <div className="overflow-hidden rounded-lg shadow-lg">
+                                        <div className="px-2">
+                                            <Image src={featureSections} className="rounded-lg" alt="featureSections" />
+                                        </div>
+                                        <div className="bg-white py-3 px-3">
+                                            <p className="text-lg font-semibold text-gray-900">Feature Sections</p>
+                                            <p className="mt-1 text-sm text-gray-500">18 components</p>
+                                        </div>
+                                    </div>
+                                </Link>
+
+
+                                <Link href="#">
+                                    <div className="overflow-hidden rounded-lg shadow-lg">
+                                        <div className="px-2">
+                                            <Image src={ctaSections} className="rounded-lg" alt="ctaSections" />
+                                        </div>
+                                        <div className="bg-white py-3 px-3">
+                                            <p className="text-lg font-semibold text-gray-900">CTA Sections</p>
+                                            <p className="mt-1 text-sm text-gray-500">11 components</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+
+                {/* Hero Sections End */}
+                {/* Elements Sections Start */}
+                <div className="mx-auto mt-12">
+                    <h2 className="mt-2 text-2xl font-bold text-center tracking-tight text-gray-900">
+                        Elements
+                    </h2>
+
+                    <div className="mt-12 px-8">
+                        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+
+                            <Link href="#">
+                                <div className="overflow-hidden rounded-lg shadow-lg">
+                                    <div className="px-2">
+                                        <Image src={Heroes} className="rounded-lg" alt="Heroes"/>
+                                    </div>
+                                    <div className="bg-white py-3 px-3">
+                                        <p className="text-lg font-semibold text-gray-900">Hero Sections</p>
+                                        <p className="mt-1 text-sm text-gray-500">12 components</p>
+                                    </div>
+                                </div>
+                            </Link>
+
+
+                            <Link href="#">
+                                <div className="overflow-hidden rounded-lg shadow-lg">
+                                    <div className="px-2">
+                                        <Image src={featureSections} className="rounded-lg" alt="featureSections"/>
+                                    </div>
+                                    <div className="bg-white py-3 px-3">
+                                        <p className="text-lg font-semibold text-gray-900">Feature Sections</p>
+                                        <p className="mt-1 text-sm text-gray-500">18 components</p>
+                                    </div>
+                                </div>
+                            </Link>
+
+
+                            <Link href="#">
+                                <div className="overflow-hidden rounded-lg shadow-lg">
+                                    <div className="px-2">
+                                        <Image src={ctaSections} className="rounded-lg" alt="ctaSections"/>
+                                    </div>
+                                    <div className="bg-white py-3 px-3">
+                                        <p className="text-lg font-semibold text-gray-900">CTA Sections</p>
+                                        <p className="mt-1 text-sm text-gray-500">11 components</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                {/* Elements Sections End */}
+            </div>
+            </div>
         </div>
-    );
+    )
+        ;
 }
+
+
+
